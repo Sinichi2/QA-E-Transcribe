a9a3b832033261ba70c112945c05e26aa3897686
Shiva Matthew Cruz <smvcruz@mymail.mapua.edu.ph>
2025-08-13 17:01:32 +0800

    updating this later

---
+++ Whisper/base_model.py
@@ -0,0 +1,12 @@
+import whisperx 
+from google.colab import userdata
+import gc
+
+model = whisperx.load_model('large-v2')
+
+TRUTHFUL_DIR = userdata.get('TRUTHFUL_DIR')
+DCEPTIVE_DIR = userdata.get('DECEPTIVE_DIR')
+
+audio_truth = whisperx.load_model()
+audio_deceptive = whisperx.load_model()
+
