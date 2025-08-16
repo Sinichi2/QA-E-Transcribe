2f91af5333bc8976334782ce4e85bd1e9337175d
Shiva Matthew Cruz <smvcruz@mymail.mapua.edu.ph>
2025-08-16 09:15:34 +0800

    Pushing this to main

--- Whisper/VAD/__init__.py
+++ Whisper/VAD/__init__.py
@@ -1,1 +1,7 @@
 #everything will run here
+from silero import silero 
+from vad import Vad
+
+__main__ = [
+    
+]
---
+++ Whisper/VAD/vad.py
@@ -0,0 +1,8 @@
+import pyannote 
+import json 
+import os 
+import sys
+
+
+class Vad(): 
+    return 'rawr'
